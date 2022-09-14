e4cc3eda2e4998510dd882a4a781b8b759c7dc71
李权威 <liquanwei@corp.netease.com>
2022-09-14 23:28:28 +0800

    feat: init

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const http = require('http');
 const ShareDB = require('sharedb');
+const koa = require('koa');
 const json1 = require('ot-json1');
 const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
 const { WebSocketServer } = require('ws');
+const { MongoClient } = require('mongodb');
+const router = require('./router');
+const db = require('sharedb-mongo')('mongodb://root:example@localhost:27017');
 
-const server = http.createServer();
-const backend = new ShareDB();
+
+const app = new koa();
+app.use(router.routes());
+app.use(router.allowedMethods());
+const server = http.createServer(app.callback());
+
+
 const wsServer = new WebSocketServer({ server });
-
 ShareDB.types.register(json1.type);
-
 wsServer.on('connection', (socket) => {
     const stream = new WebSocketJSONStream(socket);
+    const backend = new ShareDB();
     backend.listen(stream);
 });
 
