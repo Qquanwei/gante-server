338130f0ef40ccbbad8754fc2706d24c0897863e
李权威 <liquanwei@corp.netease.com>
2022-09-14 23:36:35 +0800

    feat: mongo

--- index.js
+++ index.js
@@ -8,18 +8,16 @@ const { MongoClient } = require('mongodb');
 const router = require('./router');
 const db = require('sharedb-mongo')('mongodb://root:example@localhost:27017');
 
-
 const app = new koa();
 app.use(router.routes());
 app.use(router.allowedMethods());
+
 const server = http.createServer(app.callback());
-
-
 const wsServer = new WebSocketServer({ server });
 ShareDB.types.register(json1.type);
 wsServer.on('connection', (socket) => {
     const stream = new WebSocketJSONStream(socket);
-    const backend = new ShareDB();
+    const backend = new ShareDB({ db });
     backend.listen(stream);
 });
 
